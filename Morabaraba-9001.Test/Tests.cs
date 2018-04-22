1fb99782e9789d0af6a0067d3903eceff26d57cc
Josh1N <jayngwarai@gmail.com>
2018-04-22 09:21:38 +0200

    Test stamps

--- Morabaraba-9001.Test/Tests.cs
+++ Morabaraba-9001.Test/Tests.cs
@@ -1,8 +1,198 @@
 ﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using NSubstitute;
+using System.Collections.Generic;
 
 namespace Morabaraba_9001.Test
 {
-    public class Class1
+    [TestFixture]
+    public class Test
     {
+        //Placement testing 
+        /*
+         * When the game starts, the board is empty
+         * The player with dark cows is given the first chance to move
+         * Cows can only be placed on empty spaces
+         * A maximum of 12 placements per player are allowed
+         * Cows cannot be moved during placement
+        */
+
+        [Test]
+        public void gameStart()
+        {
+
+        }
+
+        [Test]
+        public void blackPlayerStarts()
+        {
+            //arrange
+             mocked = Substitute.For<IGame>();
+            Program.g = mocked; // use the mocked thing
+            //act
+            Program.Main(new string[0]);
+            //assert
+            var whatevs_srs_yo = mocked.Received(1).Black;
+            mocked.Received().runGame(Arg.Any<Morabaraba_2.Player>());
+        }
+
+
+
+        [Test]
+        public void cowPlacementOnEmptySpace()
+        {
+            //arrange
+            IGame mocked = Substitute.For<IGame>();
+            Program.g = mocked;
+
+            //act
+            Program.Main(new string[0]);
+            //assert
+            //mocked.Received().ValidPos(Arg.Is<List<string>>(args => args[0] == Morabaraba_2.));
+        }
+
+        [Test]
+        public void twelveCowsPerPlayer()
+        {
+
+        }
+
+        [Test]
+        public void cantMovePlacedCows()
+        {
+
+        }
+
+        /*
+         * During moving,
+         * ■ A cow can only move to another connected space
+         * ■ A cow can only move to an empty space
+         * ■ Moving does not increase or decrease the number of cows on theboard
+         * 
+         */
+
+        static object[] adjacentPlace =
+        {
+
+        };
+
+
+
+        [Test]
+        public void canOnlyMoveToAdjacentPlace()
+        {
+
+            bool f = false;
+            Assert.That(f = false);
+        }
+
+        [Test]
+        public void canOnlyMoveToEmptySpace()
+        {
+            bool f = false;
+            Assert.That(f = false);
+        }
+
+        [Test]
+        public void numberOfCowsOnBoardDoesntChange()
+        {
+            bool f = false;
+            Assert.That(f = false);
+        }
+
+        //During flying,
+        //■ Cows can move to any empty space if only three cows of that colorremain
+        [Test]
+        public void canFly()
+        {
+            bool f = false;
+            Assert.That(f = false);
+        }
+
+        /*In general,
+         * ■ A mill is formed by three cows of the same color in a line
+         * ■ A mill is not formed when
+         *      ● Cows in a line are of different colors
+         *      ● Connected spaces containing cows do not form a line
+         * ■ Shooting is only possible on the turn that a mill is completed, despitethe mill persisting for another turn
+         * ■ A cow in a mill, when non-mill cows exist, cannot be shot
+         * ■ A cow in a mill, when all cows are in mills, can be shot
+         * ■ A player cannot shoot their own cows
+           ■ A player cannot shoot an empty space
+           ■ Shot cows are removed from the board
+           ■ A win occurs if an opponent cannot move
+           ■ A win occurs if an opponent has two or fewer cows, and placement isfinished 
+           */
+
+        [Test]
+        public void millsFormed()
+        {
+            bool f = false;
+            Assert.That(f = false);
+        }
+
+        [Test]
+        public void millNotFormed()
+        {
+            bool f = false;
+            Assert.That(f = false);
+        }
+
+        [Test]
+        public void onlyKillWhenMillFormed()
+        {
+            bool f = false;
+            Assert.That(f = false);
+        }
+
+        [Test]
+        public void cannotKillMillCows()
+        {
+            bool f = false;
+            Assert.That(f = false);
+        }
+
+        [Test]
+        public void canKillMIllCows()
+        {
+            bool f = false;
+            Assert.That(f = false);
+        }
+
+        [Test]
+        public void cantKillOwnCow()
+        {
+            bool f = false;
+            Assert.That(f = false);
+        }
+
+        [Test]
+        public void cannotShootEmptySpace()
+        {
+            bool f = false;
+            Assert.That(f = false);
+        }
+
+        [Test]
+        public void deadCowsRemoved()
+        {
+            bool f = false;
+            Assert.That(f = false);
+        }
+
+        [Test]
+        public void winCantMove()
+        {
+            bool f = false;
+            Assert.That(f = false);
+        }
+
+        [Test]
+        public void winLessThanTwoCows()
+        {
+            bool f = false;
+            Assert.That(f = false);
+        }
     }
 }
